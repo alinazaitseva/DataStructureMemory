828c73c733aa774f39a7afb94ae2fbd8daa49032
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-04-12 16:39:20 +0300

    create simple structure

---
+++ DataStructureMemories/DataStructureMemories/DataStructureTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataStructureTableViewController.swift
+//  DataStructureMemories
+//
+//  Created by Alina Zaitseva on 4/12/18.
+//  Copyright © 2018 Alina Zaitseva. All rights reserved.
+//
+
+import UIKit
+
+class DataStructureTableViewController: UITableViewController {
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+
+        return cell
+    }
+ 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+}
