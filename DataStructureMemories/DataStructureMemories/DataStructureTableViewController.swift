c5d6b9031c9c4f79a0c09746f565d5df234787ea
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-04-12 16:55:37 +0300

    replace indexPath from default path to array memoryList path

--- DataStructureMemories/DataStructureMemories/DataStructureTableViewController.swift
+++ DataStructureMemories/DataStructureMemories/DataStructureTableViewController.swift
@@ -9,20 +9,22 @@
 import UIKit
 
 class DataStructureTableViewController: UITableViewController {
+    
+    var memoryList = ["Stack","Queue","Set","Dequeue","Priority Queue","List(Array)", "MultiSet","Dictionary"]
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8
+        return memoryList.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        cell.textLabel?.text = memoryList[indexPath.row]
 
         return cell
     }
