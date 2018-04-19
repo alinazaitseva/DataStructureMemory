440c39f9738681beab0e625798d0efa413b10188
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-04-19 12:04:24 +0300

    remove unused functionality

--- DataStructureMemories/DataStructureMemories/ControllerDataStructure/DetailViewController.swift
+++ DataStructureMemories/DataStructureMemories/ControllerDataStructure/DetailViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Alina Zaitseva on 4/16/18.
 //  Copyright © 2018 Alina Zaitseva. All rights reserved.
 //
-
 import UIKit
 
 class DetailViewController: UIViewController {
@@ -21,36 +20,30 @@ class DetailViewController: UIViewController {
         self.descriptionValue.text = detailDescription ?? "There is no such description"
         gradientView.opacityGradient()
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     @IBOutlet weak var gradientView: UIView!
     var gradientLayer: CAGradientLayer!
-    
+
     @IBOutlet weak var halfHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var fullHeightConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     
-    @IBOutlet weak var hiddenHeightConstraint: NSLayoutConstraint!
-    
     @IBAction func buttonShowText(_ sender: Any) {
         heightConstraint.priority = UILayoutPriority(rawValue: 250)
         gradientView.isHidden = true
-//        hiddenHeightConstraint.priority = UILayoutPriority(rawValue: 999)
-//        gradientView.isHidden = false
         view.layoutIfNeeded()
     }
-    
 }
 extension UIView {
     func opacityGradient() {
         let gradient = CAGradientLayer()
         gradient.frame = self.bounds
         gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
-        gradient.locations = [0.0, 1.0, 0.0, 1.0]
+        gradient.locations = [0.0, 1.0]
         self.layer.mask = gradient
     }
 }
