68e410cf8e67211f1575176f7d61b187c5f0d28f
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-04-30 02:31:26 +0300

    added action button

--- DataStructureMemories/DataStructureMemories/VisualizateControllers/ActionButton.swift
+++ DataStructureMemories/DataStructureMemories/VisualizateControllers/ActionButton.swift
@@ -10,5 +10,23 @@ import Foundation
 import UIKit
 
 class ActionButton: UIButton {
-    var action: ( () -> () )?
+    var action: ( ()->() )?
+    init(title: String, action: ( ()->() )? = nil) {
+       super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(.yellow, for: .normal)
+        self.action = action
+        self.setupTarget()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func callToAction() {
+            action?()
+    }
+    
+    func setupTarget() {
+        self.addTarget(self, action: #selector(callToAction),for: .touchUpInside)
+    }
 }
